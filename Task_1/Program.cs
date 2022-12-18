99f874a840ee00ef2f607ec509304c11cb5ce0f3
Artemiy <birukovartemij@gmail.com>
2022-12-18 16:59:50 +0300

    2 tasks commit

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -4,9 +4,9 @@ int b = (num1 % 100) / 10;
 Console.WriteLine($"Вторая цифра - {b}");
 
 Console.WriteLine($"Второй вариант решения:");
+
 Console.WriteLine($"Введите Ваше число: ");
 
 string num2 = Convert.ToString(Console.ReadLine());
 
 Console.WriteLine($"Вот Ваша вторая цифра: {num2[1]}");
-
