c884978e34c4b49ce58ca6c306bb9155059fc83c
Artemiy <birukovartemij@gmail.com>
2022-12-18 15:03:16 +0300

    искал варианты решения

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,8 +1,4 @@
 ﻿Console.WriteLine("Введите трёхзначное число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Найдем вторую цифру Вашего числа:");
-
 int b = (num1%100)/10;
-
-Console.WriteLine($"{b}");
+Console.WriteLine($"Вторая цифра - {b}");
