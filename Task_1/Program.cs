8165499f063515a59b37055b50b6fde8f715450f
Artemiy <birukovartemij@gmail.com>
2022-12-18 15:10:43 +0300

    Закончили задачу 1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.WriteLine("Введите трёхзначное число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
-int b = (num1%100)/10;
+int b = (num1 % 100) / 10;
 Console.WriteLine($"Вторая цифра - {b}");
