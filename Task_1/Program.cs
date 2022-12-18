93c7f1d6133810a2d44725f1d105d130b0745aff
Artemiy <birukovartemij@gmail.com>
2022-12-18 16:58:00 +0300

    Добавил второй вариант решения в задачу 1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -2,3 +2,11 @@
 int num1 = Convert.ToInt32(Console.ReadLine());
 int b = (num1 % 100) / 10;
 Console.WriteLine($"Вторая цифра - {b}");
+
+Console.WriteLine($"Второй вариант решения:");
+Console.WriteLine($"Введите Ваше число: ");
+
+string num2 = Convert.ToString(Console.ReadLine());
+
+Console.WriteLine($"Вот Ваша вторая цифра: {num2[1]}");
+
