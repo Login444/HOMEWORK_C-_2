cfc69a4ab3d6ff33e02a545d38a9ba5fcf9e7411
Artemiy <birukovartemij@gmail.com>
2022-12-19 21:06:23 +0300

    задача 3 в рамках одной недели решена

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// решение задачи в рамках одной недели
+int[] array = {0, 1, 2, 3, 4, 5, 6, 7};
+// Console.WriteLine(string.Join(",", array));
+Console.WriteLine($"Какой день проверяем?");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (((array[b] % 7) == 0) || ((array[b] % 6) == 0))
+{
+    Console.WriteLine($"УРА, ВЫХОДНОЙ, МОЖЕМ ОТДЫХАТЬ!");
+}
+else
+{
+    Console.WriteLine($"Рабочий день, идем работать!(");
+}
